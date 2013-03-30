04475d0d5036de6a896fe4245d7cae31f4aff0ba
DiegoCamilo <diego.camilo@outlook.com>
2013-03-30 15:20:13 -0300

    commit dessas coisa

--- app/models/horario.rb
+++ app/models/horario.rb
@@ -1,4 +1,7 @@
 class Horario < ActiveRecord::Base
   belongs_to :professor
   attr_accessible :dia_semana, :horario1, :horario2, :horario3, :horario4, :horario5, :horario6
+  
+   symbolize :dia_semana, :in => [:segunda_feira, :terca_feira, :quarta_feira, :quinta_feira, 
+                                  :sexta_feira, :sabado, :domingo ], :scopes => true
 end
